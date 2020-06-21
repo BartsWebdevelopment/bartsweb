68e69a001ddb1b37603bcbf5c659093f27a862e3
Bart <bartswebdevelopment@gmail.com>
2020-06-21 20:58:11 +0200

    about-sectie-in ontwikkeling

--- about.php
+++ about.php
@@ -1,1 +1,23 @@
-<h1>test</h1>
+<main>
+    <section class="section-about">
+        <div class="u-center-text u-margin-bottom-big">
+            <h2 class="heading-secondary">
+                Je krijgt meer dan een website
+            </h2>
+        </div>
+        <div class="row">
+            <div class="col-1-of-2">
+                <h3 class="heading-tertiary u-margin-bottom-small">Uw website is uw online visitekaartje.</h3>
+                <p class="paragraph">
+                    Elke ondernemer heeft een unieke visie en doel om een website te laten maken. Een website is het gezicht van het bedrijf. Ik kan je helpen om een website te maken naargelang de behoeften van jouw bedrijf.
+                </p>
+                <h3 class="heading-tertiary u-margin-bottom-small">Ontwikkel uw online aanwezigheid om de concurrentie voor te blijven!</h3>
+                <p class="paragraph">
+                    U wilt graag een zelf aanpasbare professionele website. Die u zelf kan beheren en aanpassen. U kan er bevoorbeeld de laatste promoties, nieuwe producten, … aan toevoegen en wijzigen.                </p>
+            </div>
+            <div class="col-1-of-2">
+                image composition
+            </div>
+        </div>
+    </section>
+</main>
